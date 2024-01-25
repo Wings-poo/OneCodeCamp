2c428230a733afff7ab54ca94504f6cab7b01a3c
wings-poo <amandaerikalim@gmail.com>
2024-01-25 20:13:26 +0800

    Add Day 10 preparation materials

--- Day10/script.js
+++ Day10/script.js
@@ -13,30 +13,11 @@
 // Classes
 // https://www.w3schools.com/js/js_classes.asp
 
-/* (new) Array methods
-> Array.from()
-> Array.find()
-> Array.findIndex()
+/** Exercise **/
+
+/*
+links for methods:
 https://www.w3schools.com/js/js_array_methods.asp
-*/
-
-/* (new) String methods
-> String.includes()
-> String.startsWith()
-> String.endsWith()
 https://www.w3schools.com/js/js_string_methods.asp
-*/
-
-/* (new) Math methods
-> Math.trunc()
-> Math.sign()
-> Math.cbrt()
-> Math.log2()
-> Math.log10()
 https://www.w3schools.com/js/js_math.asp
-*/
-
-/* (new) Global methods
-> isFinite()
-> isNaN()
 */
